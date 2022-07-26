9a0eab39d0b5e3023c7bb30ac41b073f326004e4
Jean Cesca <jeanrcesca@gmail.com>
2022-07-26 15:50:00 -0300

    refactor: create generic  onboarding viewcontroller
    This commit adds a generic onboarding view controller that can be reused across multiple onboarding screens.

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -8,11 +8,14 @@
 import Foundation
 import UIKit
 
-class OnboardingView: UIViewController {
+class OnboardingViewController: UIViewController {
     
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
+    
+    let heroImageName: String
+    let titleText: String
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,19 +23,30 @@ class OnboardingView: UIViewController {
         style()
         configureConstraints()
     }
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
-extension OnboardingView {
+extension OnboardingViewController {
     func style() {
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.textAlignment = .center
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in the 80s."
+        label.text = titleText
         label.font = .preferredFont(forTextStyle: .title3)
         
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -48,8 +62,8 @@ extension OnboardingView {
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 2)
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 3)
         ])
     }
 }
