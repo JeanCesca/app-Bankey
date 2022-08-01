be9ce5bf9d914da00dd8f208b73405c0e10a5aca
Jean Cesca <jeanrcesca@gmail.com>
2022-08-01 16:48:00 -0300

    removido DS

--- Bankey/Bankey/Components/ShakeyBellView.swift
+++ Bankey/Bankey/Components/ShakeyBellView.swift
@@ -6,3 +6,73 @@
 //
 
 import Foundation
+import UIKit
+
+class ShakeyBellView: UIView {
+    
+    let imageView = UIImageView()
+    let buttonView = UIButton(type: .system)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 48, height: 48)
+    }
+}
+
+extension ShakeyBellView {
+    
+    func setup() {
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped))
+        imageView.addGestureRecognizer(singleTap)
+        imageView.isUserInteractionEnabled = true
+    }
+    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(systemName: "bell.fill")!.withTintColor(.white, renderingMode: .alwaysOriginal)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = image
+        
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        buttonView.backgroundColor = .systemRed
+        buttonView.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+        buttonView.layer.cornerRadius = 16/2
+        buttonView.setTitle("9", for: .normal)
+        buttonView.setTitleColor(.white, for: .normal)
+    }
+    
+    func layout() {
+        addSubview(imageView)
+        addSubview(buttonView)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 24),
+            imageView.widthAnchor.constraint(equalToConstant: 24)
+        ])
+        
+        NSLayoutConstraint.activate([
+            buttonView.topAnchor.constraint(equalTo: imageView.topAnchor),
+            buttonView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: -9),
+            buttonView.widthAnchor.constraint(equalToConstant: 16),
+            buttonView.heightAnchor.constraint(equalToConstant: 16)
+        ])
+    }
+}
+
+extension ShakeyBellView {
+    @objc func imageViewTapped() {
+        print("tapped")
+    }
+}
